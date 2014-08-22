9d893895e2d669425f20f34e224ad72bc16c2817
Connor Hindley <conn.hindley@gmail.com>
2014-08-22 18:59:35 -0400

    trim more fat

--- Gruntfile.js
+++ Gruntfile.js
@@ -265,29 +265,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // The following *-min tasks produce minified files in the dist folder
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/public/assets/images'
-        }]
-      }
-    },
-
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.svg',
-          dest: '<%= yeoman.dist %>/public/assets/images'
-        }]
-      }
-    },
-
     // Allow the use of non-minsafe AngularJS files. Automatically makes it
     // minsafe compatible so Uglify does not destroy the ng references
     ngAnnotate: {
@@ -388,9 +365,7 @@ module.exports = function (grunt) {
       },
       dist: [
         'jade',
-        'sass',
-        'imagemin',
-        'svgmin'
+        'sass'
       ]
     },
 
